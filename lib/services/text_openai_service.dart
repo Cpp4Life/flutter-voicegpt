c889b8e020d1a8fc37f2a98f1a81fcfdb2f7aff9
Dat Truong <dattruong@Dats-MacBook-Pro.local>
2023-03-31 22:42:40 +0700

    add chat history

--- lib/services/text_openai_service.dart
+++ lib/services/text_openai_service.dart
@@ -2,6 +2,8 @@ import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TextOpenAIService {
+  final List<Map<String, String>> _history = [];
+
   final _openAI = OpenAI.instance.build(
     token: dotenv.env['TOKEN'],
     baseOption: HttpSetup(
@@ -13,17 +15,31 @@ class TextOpenAIService {
   );
 
   Future<String> getResponse(String message) async {
+    final userInput = Map<String, String>.of(
+      {
+        "role": "user",
+        "content": message,
+      },
+    );
+    _history.add(userInput);
+
     try {
       final request = ChatCompleteText(
-        messages: [
-          Map.of({"role": "user", "content": message})
-        ],
+        messages: _history,
         model: kChatGptTurbo0301Model,
         maxToken: 1024,
       );
       final ChatCTResponse? response = await _openAI.onChatCompletion(request: request);
       if (response != null) {
-        return response.choices[0].message.content;
+        final responseMsg = response.choices[0].message;
+        final assistantInput = Map<String, String>.of(
+          {
+            "role": "assistant",
+            "content": responseMsg.content,
+          },
+        );
+        _history.add(assistantInput);
+        return responseMsg.content;
       }
       return 'Oops! Something went wrong';
     } catch (error) {
